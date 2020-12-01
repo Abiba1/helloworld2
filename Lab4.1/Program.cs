087335b00aa5183251c1a057b1defb93c811b245
321 123 <none>
2020-12-02 03:45:13 +0700

    main program

--- Lab4.1/Program.cs
+++ Lab4.1/Program.cs
@@ -6,7 +6,30 @@ namespace Lab4._1
 	{
 		static void Main(string[] args)
 		{
-			
+			while (true)
+			{
+				PrintMenu();
+				DoAction(Console.ReadLine());
+			}
+		}
+
+		static void PrintMenu() =>
+			Console.WriteLine("1: Input A\n2: Input B\n3: +\n4: -\n 5: *\n6: /");
+
+		static void DoAction(string input)
+		{
+			switch (input)
+			{
+				case "1":
+				case "2":
+				case "3":
+				case "4":
+				case "5":
+				case "6":
+				default:
+					Console.WriteLine("Unrecognized command.");
+					break;
+			}
 		}
 	}
 }
