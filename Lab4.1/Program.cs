a03b3f3c73d06d45293b781ca9a31d261c2a6795
Aboba5 <levisovdmitrij@gmail.com>
2022-12-06 19:38:56 +0700

    1

--- Lab4.1/Program.cs
+++ Lab4.1/Program.cs
@@ -34,7 +34,7 @@ namespace Lab4._1
 					Console.WriteLine("Unrecognized command.");
 					break;
 			}
-			Console.WriteLine("Попытка 9");
+			Console.WriteLine("Попытка 10");
 		}
 
 		static void Plus(double a, double b) => Console.WriteLine(a + b);
